3afdbf47aa4042c15ff17103f804dc693eb3ffd9
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-23 00:47:43 -0600

    environ

--- integradora/settings.py
+++ integradora/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default ='django-insecure-2mxyeo!nf%yw-uk%z&bt!42r6&n=4d86u^l4f)$aduts5ncr)%')
+SECRET_KEY = os.environ.get('SECRET_KEY', default ='django-insecure-2mxyeo!nf%yw-uk%z&bt!42r6&n=4d86u^l4f)$aduts5ncr)%')
 
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.enviroment.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
