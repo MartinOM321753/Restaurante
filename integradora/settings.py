3df6c55d5aa16d844de65c249b67e70277465d16
MartinOM321753 <133397788+MartinOM321753@users.noreply.github.com>
2025-04-23 02:08:05 -0600

    sqlite

--- integradora/settings.py
+++ integradora/settings.py
@@ -95,18 +95,15 @@ WSGI_APPLICATION = 'integradora.wsgi.application'
 import pymysql
 pymysql.install_as_MySQLdb()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'integradora',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
+
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
